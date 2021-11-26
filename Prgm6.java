6af3bf68e27d0c0455f1e5b9ad1248399c2dd945
Shreyayerraballa <shreyayerraballa@gmail.com>
2021-11-26 18:17:27 +0530

    first commit

---
+++ Prgm6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Prgm6{
+public static void main(String[] args){
+    int result;
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++){
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int n=sc.nextInt();
+        result=a;
+        for(int j=0;j<=n-1;j++){
+            result+=(((Math.pow(2,j))*b));
+            System.out.print(" "+result);
+        }
+    }
+    sc.close();
+}
+}
