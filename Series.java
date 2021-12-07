f3fc5407761117a18bbb60d5fafd4c1716bca8f0
Shreyayerraballa <shreyayerraballa@gmail.com>
2021-12-07 17:26:51 +0530

    first commit

---
+++ Series.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.*;
+public class Series{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int n=sc.nextInt();
+            
+
+
+
+        }
+
+
+    }
+}
