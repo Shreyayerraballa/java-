d2d264bf36f3d087fadf46ca43a6f6c1fededbc8
Shreyayerraballa <shreyayerraballa@gmail.com>
2021-12-07 20:05:37 +0530

    first commit

---
+++ Substring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Substring {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String str =sc.next();
+        int start=sc.nextInt();
+        int end=sc.nextInt();
+        System.out.println(str.substring(start,end));
+        sc.close();
+    }
+    
+}
